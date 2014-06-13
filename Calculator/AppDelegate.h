4c863e8a4ce66691a5f3b21f0619a297c0aba4f5
Kevin Kwon <myhhkb@gmail.com>
2014-06-13 23:28:27 +0900

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by hdk on 2014. 6. 13..
+//  Copyright (c) 2014년 Kevin Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by hdk on 2014. 6. 13..
+//  Copyright (c) 2014년 Kevin Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
